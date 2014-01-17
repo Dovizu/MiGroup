6633d5dba80887fe8c93f9f854e5c8ccd7400664
Donny Reynolds <donnywith@gmail.com>
2014-01-16 23:56:01 -0800

    implemented chat log using NSCollectionView

---
+++ GroupOSX/DNChatLogSingleView.h
@@ -0,0 +1,13 @@
+//
+//  DNChatLogSingleView.h
+//  GroupOSX
+//
+//  Created by Donny Reynolds on 1/16/14.
+//  Copyright (c) 2014 Dovizu Network. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DNChatLogSingleView : NSView
+
+@end
